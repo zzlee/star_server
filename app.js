097ba30c76af0e570240ef533634a21837ba7d7b
jeff <jeff@feltmeng.com>
2013-02-22 11:42:45 +0800

    jeff_getTimeData

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ global.logger = logger;
   
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 80);
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -209,6 +209,9 @@ setTimeout(function(){
 
 }, 5000);
 */	
+
+//JF
+app.post('/internal/dooh_timeslot_rawdata', routes.timeDataGet);
 
 
 //test of Jeff
