f65c8955eb4a22050e67486e3005835be1d22d88
test_server <dev@feltmeng.com>
2013-02-23 11:45:37 +0800

    update

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ global.logger = logger;
   
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
