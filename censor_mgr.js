6133e84bd03667ad0e7e0fd9b0fb1ea40eccb8a0
kaiser <kaiser@feltmeng.com>
2013-06-25 19:19:47 +0800

    temp version(implement getUGCList,getUserContent and setUGCAttribute)

--- censor_mgr.js
+++ censor_mgr.js
@@ -2,6 +2,10 @@
 var censorMgr = {};
 
 var globalConnectionMgr = require('./global_connection_mgr.js');
+var fb_handler = require('./fb_handler.js');
+var FMDB = require('./db.js');
+var UGCs = FMDB.getDocModel("ugc");
+
 
 /**
  * @param  request
@@ -18,37 +22,19 @@ var globalConnectionMgr = require('./global_connection_mgr.js');
  *                       rating(Range A~E),
  *                       doohPlayedTimes}
  */
-
-
 var getUGCList = function(condition,get_cb){
-    var UGC_mgr = require('./UGC.js');
-    var FMDB = require('./db.js');
-    
-    var UGCs = FMDB.getDocModel("ugc");
- //test   
-    var start = new Date('Jun 26, 2013');
-    var end = new Date('Jun 26, 2013');
-    var doohPlayedTimes = 0;
-    var rating = 'a';
-    
-//    condition = {'createdOn': {$gte: start, $lt: end},
-//                 'doohPlayedTimes':doohPlayedTimes,
-//                 'rating':rating
-//    };
-//    
+
     FMDB.listOfdocModels( UGCs,condition,'fb.userName fb.userID _id title description createdOn rating doohPlayedTimes', {sort:{createdOn: -1}}, function(err, result){
         if(err) {logger.error('[db.listOfUGCs]', err);
-                get_cb(err);
+            get_cb(err,null);
         }
         if(result){
-
-                limit = result.length;
-                
-//                console.log("listOfdocModels_result"+result);
-                get_cb(limit);
+            limit = result.length;
+//          console.log("listOfdocModels_result"+result);
+            get_cb(null,limit);
         }
     });
-    
+
 
     /**test
     var vjson = {
@@ -59,16 +45,25 @@ var getUGCList = function(condition,get_cb){
         if(err)  console.log("createAdoc_err"+err);
         if(result) console.log("createAdoc_res"+result);
     });
-    */
+     */
 
 };//getUGCList end
 
 
 //kaiser test
-getUGCList(null,function(err,res){
-    if(err) console.log("getUGCList"+err);
-    else console.log("getUGCList"+res);
-    
+var condition;
+var start = new Date('Jun 25, 2013');
+var end = new Date('Jun 26, 2013');
+var doohPlayedTimes = 0;
+var rating = 'd';
+condition = {'createdOn': {$gte: start, $lt: end},
+'doohPlayedTimes':doohPlayedTimes,
+'rating':rating
+};
+getUGCList(condition,function(err,res){
+    if(err) console.log("getUGCList_err="+err);
+    else console.log("getUGCList="+res);
+
 });
 
 
@@ -84,7 +79,7 @@ getUGCList(null,function(err,res){
  * 
  */
 var getTimeslots = function(get_cb){
-    
+
 };
 
 
@@ -96,22 +91,58 @@ var getTimeslots = function(get_cb){
  * @return response json{FBProfilePicture(link)}
  *                       
  */
-var getUserContent = function(get_cb){
-    
-};
+var getUserContent = function(fb_id,get_cb){
 
+    fb_handler.getUserProfilePicture(fb_id,function(err, result){
+        if(err)
+            get_cb(JSON.stringify(err),null);
+//            console.log("err: " + JSON.stringify(err));
+        else
+            get_cb(null,JSON.stringify(result));
+//            console.log("result: "+JSON.stringify(result));
+    });
+
+};
+//kaiser test
+var fb_id = '100005962359785';
+getUserContent(fb_id,function(err,res){
+    if(err) console.log("getUserContent_err="+err);
+    else console.log("getUserContent="+res);
+
+});
 
 /**
- * @param  request  {string}projectId
+ * @param  request  {string}_id
  * 
  *         body     {string}UGCLevel(Range A~E)    
  *                  
  * @return response {string}status 
  *                       
  */
-var setUGCAttribute = function(get_cb){
-    
+var setUGCAttribute = function(_id,vjson,get_cb){
+
+    FMDB.updateAdoc(UGCs,_id,vjson, function(err, result){
+        if(err) {logger.error('[db.updateAdoc]', err);
+          get_cb(err,null);
+        }
+        if(result){
+//          console.log("updateAdoc_result"+result);
+          get_cb(null,result.rating);
+        }
+    });
+
 };
+
+//kaiser test
+var vjson = {
+        rating : 'd',
+};
+var _id = '51c939f65fcfaf8823000002';
+setUGCAttribute(_id,vjson,function(err,res){
+    if(err) console.log("setUGCAttribute_err"+err);
+    else console.log("setUGCAttribute_change="+res);
+
+});
 
 
 module.exports = censorMgr;
