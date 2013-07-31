5ff99c2c5b24db537fea2617fadf8dece074fabf
test_server <dev@feltmeng.com>
2013-07-31 18:58:10 +0800

    device token updated

--- routes/authorization_handler.js
+++ routes/authorization_handler.js
@@ -18,15 +18,24 @@ FM.authorizationHandler.checkAuth = function(req, res, next) {
 //PUT /members/:memgerId/device_tokens
 FM.authorizationHandler.updateDeviceToken = function(req, res) {
 	//update device token from android devices
-//	logger.info('[PUT ' + req.path + '] is called');
-	console.dir(req.body);
-//	console.log('member Id :' + req.params.memberId);
-//	if(req.body){
-//		
-//	}
+	logger.info('[PUT ' + req.path + '] is called');
 	
-	
-	
+	if(req.body){
+	    FM_LOG("\n[Got Device_Token] " + res.body.deviceToken);
+	    var oid = ObjectID.createFromHexString(req.params.memberId);
+	    var jsonStr = '{"deviceToken.' + res.body.platform +'":"'+ res.body.deviceToken + '"}';
+	    
+        var json = JSON.parse(jsonStr);
+        memberDB.updateMember( oid, json, function(err, result){
+            if(err) logger.info(err);
+            if(result) logger.info(result);
+//            res.send({"message": "Update Device Token!"});
+            res.send(200);
+        });
+	}else{
+	    res.send(400);
+//	    res.send({"message": "Failed!"});
+	}
 	
 	
 };
