db37e600d70b31b95904d704429f96c7fe059e9f
Gance <gance@feltmeng.com>
2013-08-27 10:58:29 +0800

    Fixed the bug of having "POST /members/fb_info" and "GET /connectStarServer" do the auth check

--- app.js
+++ app.js
@@ -90,8 +90,8 @@ app.get('/fb/thumbnail', routes.api.fbGetThumbnail);
 app.get('/members/authentication_code', routes.authorizationHandler.checkAuth, routes.api.codeGenerate);
 app.post('/members/authentication_code_validity', routes.authorizationHandler.checkAuth, routes.api.codeVerify);  //TODO: better use GET
 app.get('/members/fb_token_validity', routes.authorizationHandler.checkAuth, routes.member.isFBTokenValid);
-app.post('/members/fb_info', routes.authorizationHandler.checkAuth, routes.api.signupwithFB);
-app.get('/connectStarServer', routes.authorizationHandler.checkAuth, routes.api.connection);  //Make sure client side connect connect star_server
+app.post('/members/fb_info', routes.api.signupwithFB);
+app.get('/connectStarServer', routes.api.connection);  //Make sure client side connect connect star_server
 
 
 //PUT /members/{_id}/device_tokens 
