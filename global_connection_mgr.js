706292643d7f73d5549962764792d6904d42eed0
Gance <gance@feltmeng.com>
2013-09-10 15:23:58 +0800

    Long polling handling by star_coordinator works!

--- global_connection_mgr.js
+++ global_connection_mgr.js
@@ -22,44 +22,44 @@ FM.globalConnectionMgr = (function(){
                 delete connectedRemotes[remoteID];
             },
             
-            getConnectedRemotes: function(type, cb){ //deprecated
-                if ( (config.IS_STAND_ALONE=="yes")||(config.IS_STAND_ALONE=="Yes")||(config.IS_STAND_ALONE=="YES") ) {
-                    var result = new Array();
-                    //console.log('total connections');
-                    
-                    for (anId in connectedRemotes){
-                        //console.log('%s %s', anId, connectedRemotes[anId]);
-                        if (type){
-                            if (connectedRemotes[anId].type==type){
-                                result.push(anId);
-                            }                        
-                        }
-                        else { //push all the items in connectedRemotes
-                            result.push(anId);
-                        }
-                    }
-                    
-                    cb(null, result);
-                }
-                else { //TODO: star_server has multiple instances (due to auto-scale of AWS).  This section of code is NOT verified yet. 
-                    request({
-                        method: 'GET',
-                        uri: config.HOST_STAR_COORDINATOR_URL + '/internal/connected_remotes',
-                        qs: {"type": type},
-                        json: true
-                        
-                    }, function(error, response, body){
-
-                        if (body) {
-                            cb(null, body.connectedRemotes);    
-                        }
-                        else {
-                            cb("Failed to get connected remotes from star_coordinator: "+error, null);  //TODO: check the content of error parameters
-                        }
-                                
-                    });
-                }
-            },
+//            getConnectedRemotes: function(type, cb){ //DEPRECATED
+//                if ( (config.IS_STAND_ALONE=="yes")||(config.IS_STAND_ALONE=="Yes")||(config.IS_STAND_ALONE=="YES") ) {
+//                    var result = new Array();
+//                    //console.log('total connections');
+//                    
+//                    for (anId in connectedRemotes){
+//                        //console.log('%s %s', anId, connectedRemotes[anId]);
+//                        if (type){
+//                            if (connectedRemotes[anId].type==type){
+//                                result.push(anId);
+//                            }                        
+//                        }
+//                        else { //push all the items in connectedRemotes
+//                            result.push(anId);
+//                        }
+//                    }
+//                    
+//                    cb(null, result);
+//                }
+//                else { //TODO: star_server has multiple instances (due to auto-scale of AWS).  This section of code is NOT verified yet. 
+//                    request({
+//                        method: 'GET',
+//                        uri: config.HOST_STAR_COORDINATOR_URL + '/internal/connected_remotes',
+//                        qs: {"type": type},
+//                        json: true
+//                        
+//                    }, function(error, response, body){
+//
+//                        if (body) {
+//                            cb(null, body.connectedRemotes);    
+//                        }
+//                        else {
+//                            cb("Failed to get connected remotes from star_coordinator: "+error, null);  //TODO: check the content of error parameters
+//                        }
+//                                
+//                    });
+//                }
+//            },
             
             getConnectedRemoteWithLowestLoad: function(type, cbOfGetConnectedRemoteWithLowestLoad){
                 debugger;
@@ -83,8 +83,23 @@ FM.globalConnectionMgr = (function(){
                     
                     cbOfGetConnectedRemoteWithLowestLoad(null, connectedRemoteWithLowestLoad);
                 }
-                else { //TODO: star_server has multiple instances (due to auto-scale of AWS)
+                else { //star_server has multiple instances (due to auto-scale of AWS)
+                    request({
+                        method: 'GET',
+                        uri: config.HOST_STAR_COORDINATOR_URL + '/internal/connected_remote_with_lowest_load',
+                        qs: {"type": type},
+                        json: true
+                        
+                    }, function(error, response, body){
                     
+                        if (body) {
+                            cbOfGetConnectedRemoteWithLowestLoad(null, body.connectedRemoteWithLowestLoad);    
+                        }
+                        else {
+                            cbOfGetConnectedRemoteWithLowestLoad("Failed to get connected remotes from star_coordinator: "+error, null);  //TODO: check the content of error parameters
+                        }
+                                
+                    });
                 }
             },
             
@@ -97,12 +112,28 @@ FM.globalConnectionMgr = (function(){
                 }
             },
             
-            sendRequestToRemote: function( targetedRemoteID, reqToRemote, cb ) {
+            sendRequestToRemote: function( targetedRemoteID, reqToRemote, cbOfSendRequestToRemote ) {
                 if ( (config.IS_STAND_ALONE=="yes")||(config.IS_STAND_ALONE=="Yes")||(config.IS_STAND_ALONE=="YES") ) {
-                    connectionHandler.sendRequestToRemote( targetedRemoteID, reqToRemote, cb );
+                    connectionHandler.sendRequestToRemote( targetedRemoteID, reqToRemote, cbOfSendRequestToRemote );
                 }
-                else { //TODO: star_server has multiple instances (due to auto-scale of AWS)
+                else { //star_server has multiple instances (due to auto-scale of AWS)
+                    request({
+                        method: 'POST',
+                        uri: config.HOST_STAR_COORDINATOR_URL + '/internal/requests_to_remote',
+                        body: {"targetedRemoteID": targetedRemoteID, "reqToRemote": reqToRemote},
+                        json: true
+                        
+                    }, function(error, response, body){
                     
+                        if (body) {
+                            cbOfSendRequestToRemote(null, body.responseParameters);    
+                        }
+                        else {
+                            cbOfSendRequestToRemote("Failed to send request to remote: "+error, null);  //TODO: check the content of error parameters
+                        }
+                                
+                    });
+
                 }
             }
             
