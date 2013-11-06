f9c9956c261b75f30b8841c32206250bb4f3f892
Joy Chen <joy@feltmeng.com>
2013-11-06 11:50:16 +0800

    add pic text

--- public/webapp/template_pic_text_pg.js
+++ public/webapp/template_pic_text_pg.js
@@ -1,4 +1,4 @@
-FmMobile.template_pic_text_pg = {
+﻿FmMobile.template_pic_text_pg = {
 	//  Page constants.
 PAGE_ID: "template_pic_text_pg",
     
@@ -18,19 +18,7 @@ show: function(){
     
 load: function(event, data){
     
-	document.addEventListener("showkeyboard", function() {
-		 if(device.platform != "Android"){
-		 }else{
-		 	$("#nav-bar").hide();
-		 }
-}, false);
-
-document.addEventListener("hidekeyboard", function() {
-        if(device.platform != "Android"){
-		 }else{
-		 	$("#nav-bar").show();
-		 }
-}, false);
+	
     
 
    // FmMobile.userContent.text="aa";
@@ -53,7 +41,8 @@ document.addEventListener("hidekeyboard", function() {
                         moreLineInBox=$("#ur_text").val().replace(/\n/g,"<n>");
                         var moreLineInBox_2=moreLineInBox.split("<n>");
                         if(moreLineInBox_2.length>3){
-                        FmMobile.showNotification("inAreaTextOver");
+                        //FmMobile.showNotification("inAreaTextOver");
+                        	alert("超過三行囉!第四行會超過大螢幕");
                         $("#ur_text").val(moreLineInBox_2[0]+"\n"+moreLineInBox_2[1]+"\n"+moreLineInBox_2[2]);
                         return false;
                         }
@@ -65,7 +54,60 @@ document.addEventListener("hidekeyboard", function() {
     $("#back_main").click(function(){
                           $.mobile.changePage("template-sub_template.html");
                           });
-    
+    $("#go_cropper").click(function(){
+    	//------------- handle text ------------
+    	 if($("#ur_text").val().length==0 ||$("#ur_text").val()==" "){
+             alert("請至少寫一個字");
+         }else{
+             
+             var check_format= FmMobile.userContent.text.split("<n>");
+             //FmMobile.userContent.text=check_format;
+             if(check_format.length>3){
+                 
+                 return false;
+                 //alert("more than 4 lines!");
+             }
+             
+             if(check_format[0].length >8){
+                 
+               //  FmMobile.showNotification("moreWords");
+                 return false;
+                 
+                 
+             }
+             if(check_format[1] != undefined){
+                 if(check_format[1].length >8 ){
+                  //   FmMobile.showNotification("moreWords");
+                     return false;
+                     
+                 }
+             }
+             
+             if(check_format[2] != undefined){
+                 if(check_format[2].length >8 ){
+                    // FmMobile.showNotification("moreWords");
+                     return false;
+                     
+                 }
+             }
+
+         }
+    	
+    	//------------------------
+    	
+    	
+    	
+    	
+	      if(localStorage.imgForCropper == undefined){
+		  alert("請選張圖吧");
+		  return false;
+		  }
+	   
+	   fileProcessedForCropperURI = localStorage.imgForCropper;
+	   FmMobile.userContent.picture.urlOfOriginal = localStorage.imgForCropper;
+      $.mobile.changePage("template-photo_cropper.html");
+    });
+	  
     
     $('#template_top_img_text_pic').attr({src:FmMobile.selectedTemplateBarImg});
     
@@ -78,122 +120,40 @@ document.addEventListener("hidekeyboard", function() {
     
     var itemContentIsReady;
     
+    //-------------------------------------------------------------------
+	
+	  function handleFileSelect(evt) {
+var files = evt.target.files; // FileList object
+
+var reader = new FileReader();
+
+// Closure to capture the file information.
+reader.onload = (function(theFile) {
+  return function(e) {
+    // Render thumbnail.
+    var span = document.createElement('span');
+    span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                      '" title="', escape(theFile.name), '"/>'].join('');
+      
+      //alert("ww");
+    localStorage.setItem('imgForCropper', e.target.result);
+                document.getElementById('list').src= localStorage.imgForCropper;
+
+  };
+})(files[0]);
+
+// Read in the image file as a data URL.
+reader.readAsDataURL(files[0]);
+// }
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+	//------------------------------------------------------------------
     
-    
-    var buttonClick_cb = function(event, ui) {
-        
-        console.log('button clicked!');
-        //fileObjectID = event.data.objectID;
-        //console.log('[buttonClick_cb()] fileObjectID = %s', fileObjectID);
-        //alert('fileObjectID = '+fileObjectID );
-        if($("#ur_text").val().length==0 ||$("#ur_text").val()==" "){
-            FmMobile.showNotification("nullText");
-        }else{
-            
-            var check_format= FmMobile.userContent.text.split("<n>");
-            //FmMobile.userContent.text=check_format;
-            if(check_format.length>3){
-                FmMobile.showNotification("moreLines");
-                return false;
-                //alert("more than 4 lines!");
-            }
-            
-            if(check_format[0].length >8){
-                
-                FmMobile.showNotification("moreWords");
-                return false;
-                
-                
-            }
-            if(check_format[1] != undefined){
-                if(check_format[1].length >8 ){
-                    FmMobile.showNotification("moreWords");
-                    return false;
-                    
-                }
-            }
-            
-            if(check_format[2] != undefined){
-                if(check_format[2].length >8 ){
-                    FmMobile.showNotification("moreWords");
-                    return false;
-                    
-                }
-            }
-        
-        var getPhotoFail = function (message) {
-            //alert('�⊥��詨����������活��);
-        }
-        
-        
-        var gotoPhotoCropper = function (imageURI) {
-            
-            FmMobile.userContent.picture.urlOfOriginal = imageURI;
-            
-            if ( (device.version > "6") && (device.platform != "Android")) {
-                
-                //Here is the workaround for iOS 6.0 and 6.0.1 subsampling issue (when drawing from a more-than-2M jpg to canvas)
-                var tempImg = new Image();
-                tempImg.src = imageURI;
-                tempImg.onload = function() {
-                    EXIF.getData( tempImg, function(){
-                                 var orientation = EXIF.getTag(tempImg, "Orientation");
-                                 subsamplingResize(imageURI, { maxWidth: 960, maxHeight: 960, orientation: orientation }, function(resultURI){
-                                                   fileProcessedForCropperURI = resultURI;
-                                                   $.mobile.changePage("template-photo_cropper.html");
-                                                   });
-                                 });
-                    
-                };
-                
-                
-            }
-            else {
-               var tempImg = new Image();
-                    tempImg.src = imageURI;
-                    tempImg.onload = function() {
-                        EXIF.getData( tempImg, function(){
-                                     var orientation = EXIF.getTag(tempImg, "Orientation");
-                                     subsamplingResize(imageURI, { maxWidth: 960, maxHeight: 960, orientation: orientation }, function(resultURI){
-                                                       fileProcessedForCropperURI = resultURI;
-                                                       $.mobile.changePage("template-photo_cropper.html");
-                                                       });
-                                     });
-                        
-                    }; 
-                
-            }
-            
-            console.log("version="+device.version);
-            
-        };
-        
-        if ( event.data.photoSource == "album" ) {
-            navigator.camera.getPicture(gotoPhotoCropper, getPhotoFail,{
-                                        quality: 50,
-                                        destinationType: navigator.camera.DestinationType.FILE_URI,
-                                        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY,
-                                        targetWidth: 1500,
-                                        targetHeight: 1500
-                                        });
-            //FmMobile.analysis.trackEvent("Button", "Click", "album", 1);
-        }
-        else {
-            navigator.camera.getPicture(gotoPhotoCropper, getPhotoFail,{
-                                        quality: 50,
-                                        destinationType: navigator.camera.DestinationType.FILE_URI,
-                                        sourceType: navigator.camera.PictureSourceType.CAMERA,
-                                        targetWidth: 1500,
-                                        targetHeight: 1500
-                                        });
-            //FmMobile.analysis.trackEvent("Button", "Click", "camera", 1);
-        }
-    
-        }
-    };
-    
-    $('#btnUseCamera').bind( "click", { photoSource: "camera" }, buttonClick_cb);
-    $('#btnUseAlbum').bind( "click", { photoSource: "album" }, buttonClick_cb);
+  
+   // $('#btnUseCamera').bind( "click", { photoSource: "camera" }, buttonClick_cb);
+   // $('#btnUseAlbum').bind( "click", { photoSource: "album" }, buttonClick_cb);
     
     
 }
