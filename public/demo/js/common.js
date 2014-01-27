313ee9a41bcbb3b9e1c504fc3d499a6b9bc9402c
Jean <jean@feltmeng.com>
2014-01-27 10:18:21 +0800

    Changed IP in /demo

--- public/demo/js/common.js
+++ public/demo/js/common.js
@@ -1,8 +1,8 @@
 //var serverUrl = "http://joy.ondascreen.com/demo";
-var serverUrl = "http://jean.ondascreen.com/demo";
-//var serverUrl = "http://internal.ondascreen.com/demo";
+//var serverUrl = "http://jean.ondascreen.com/demo";
+var serverUrl = "http://internal.ondascreen.com/demo";
 /** --------------------------------------------------- */
 
-//var domainUrl = "http://internal.ondascreen.com";
+var domainUrl = "http://internal.ondascreen.com";
 //var domainUrl = "http://joy.ondascreen.com";
-var domainUrl = "http://jean.ondascreen.com";
+//var domainUrl = "http://jean.ondascreen.com";
