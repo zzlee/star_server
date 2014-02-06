26de7da01543b5a456e17516c10e19fdc6f17ff9
Kaiser <kaiser@feltmeng.com>
2014-02-06 18:51:46 +0800

    change url to www.ondascreen.com

--- public/wls/js/common.js
+++ public/wls/js/common.js
@@ -1,8 +1,8 @@
-var serverUrl = "http://joy.ondascreen.com/wls";
+var serverUrl = "http://www.ondascreen.com/wls";
 //var serverUrl = "http://jean.ondascreen.com/wls";
 //var serverUrl = "http://internal.ondascreen.com/wls";
 /** --------------------------------------------------- */
 
 //var domainUrl = "http://internal.ondascreen.com";
-var domainUrl = "http://joy.ondascreen.com";
+var domainUrl = "http://www.ondascreen.com";
 //var domainUrl = "http://jean.ondascreen.com";
